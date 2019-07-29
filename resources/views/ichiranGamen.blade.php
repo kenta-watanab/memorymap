5bb7708c245946b777b027611c322f7aaa88b6a4
ikeajinbe <kenta23432486@yahoo.co.jp>
2019-07-29 22:58:02 +0900

    '一覧画面'

--- resources/views/ichiranGamen.blade.php
+++ resources/views/ichiranGamen.blade.php
@@ -7,37 +7,35 @@
 
 @section('content')
 
-<div class="container">
-  <div class="inline">
+<div class="b_container">
 
-    <table>
-
-      <tbody>
+  @if (count($data) < 0)
+    <div>
+        <p>登録されていません</p>
+    </div>
+    @endif
 
         <script>
           <!--コントローラーから渡された$dataを取得 -->
           var data = @json($data);
           var images = @json($images);
-          for (var i = 0; i < data.length; i++) 
-          { var pass="toShosaiGamen(" + data[i]['id'] + ")" ; var tag="<tr class='border' onclick=" + pass + ">" document.write(tag); document.write("<td class='hidden'>");
-          document.write(data[i]['id']);
-          document.write("</td>");
 
-          document.write("<td>");
-            document.write(data[i]['place_date']);
-            document.write("</td>");
+          for (var i = 0; i < data.length; i++) { 
+            
+            var pass="toShosaiGamen(" + data[i]['id'] + ")" ; 
+            
+            var tag="<div class ='list' onclick=" + pass + ">" 
+            document.write(tag);
+              
+              document.write("<img src=");
+              document.write(images[i]);
+              document.write(" class='file'>");
+           
+              document.write(data[i]['place_date']);           
+              document.write(data[i]['place_name']);
 
-          document.write("<td>");
-            document.write(data[i]['place_name']);
-            document.write("</td>");
-
-          document.write("<td>");
-            document.write("<img src=");
-                 document.write(images[i]);
-                 document.write(" class='file'>");
-            document.write("</td>");
-
-          document.write("</tr>")
+            document.write("</div>");
+            
           }
 
           function toShosaiGamen(id) {
@@ -45,12 +43,6 @@
           }
 
         </script>
-
-      </tbody>
-
-    </table>
-
-  </div>
 </div>
 
 @endsection
